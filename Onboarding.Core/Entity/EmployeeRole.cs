43a54ae7853cfcbae59fe3b72e62406860e02003
H13062 <bgh1506@gmail.com>
2023-04-29 15:30:30 -0500

    add userrole authenthication API

---
+++ Onboarding.Core/Entity/EmployeeRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Onboarding.Core.Entity
+{
+    public class EmployeeRole
+    {
+        [Key]
+        public int LookupCode { get; set; }
+        public string? Name { get; set; }
+        public string? ABBR { get; set; }
+        public List<Employee>? Employees { get; set; }
+    }
+}
